81d599660f29b40f36a01ad1c45761dca610c17e
Babatunde Lamidi <odunayohlamidi@gmail.com>
2025-01-01 20:18:14 +0100

    custom input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgCustomInputComponent } from './ng-custom-input/ng-custom-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgCustomInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'custom-input';
+  username = signal('');
+  password = signal('');
+  pin = signal('');
 }
---
+++ src/app/ng-custom-input/ng-custom-input.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, input, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+export type InputType = 'text' | 'password' | 'number' | 'tel'
+export type IconPosition = 'front' | 'back'
+
+@Component({
+  selector: 'app-ng-custom-input',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './ng-custom-input.component.html',
+  styleUrl: './ng-custom-input.component.css'
+})
+export class NgCustomInputComponent {
+
+  type = input<InputType>('text')
+  icon = input<string | undefined>(undefined)
+  iconPosition = input<IconPosition>('front');
+  placeholder = input<string>('')
+  maxLength = input<number | undefined>(undefined)
+  minLength = input<number | undefined>(undefined)
+  disabled = input<boolean>(false)
+
+  modelValue = model<string>('')
+
+  private showPasswordSignal = signal(false)
+  private errorMessageSignal = signal<string | undefined>(undefined)
+
+  protected showPassword = computed(() => this.showPasswordSignal())
+  protected errorMessage = computed(() => this.errorMessageSignal())
+  protected error = computed(() => !!this.errorMessageSignal())
+
+
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value;
+    if (this.type() === 'number') {
+      const numericValue = value.replace(/[^0-9]/g, '');
+      if (this.maxLength()) {
+        input.value = numericValue.slice(0, this.maxLength());
+        this.modelValue.set(input.value);
+      }
+    } else {
+      this.modelValue.set(value);
+    }
+  }
+
+
+  togglePassword(): void {
+    this.showPasswordSignal.update(value => !value);
+  }
+
+
+
+
+}
